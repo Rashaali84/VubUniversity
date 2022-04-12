5d8487e0b28b59d4524df4fea21a831e3477629c
Rasha Ali <rashaali84@gmail.com>
2022-04-12 12:34:55 +0200

    chnages to code

---
+++ Models/EmailReputaionModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using VubUniversity.Services;
+
+namespace VubUniversity.Models
+{
+   
+     public class EmailReputaionModel : ValidationAttribute
+    {
+        public string GetErrorMessage() => "Email address is rejected because of its reputation";
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string email = value.ToString();
+            var service = (IEmailCheckRisky)validationContext.GetService(typeof(IEmailCheckRisky));
+            if (service.IsUntrusted(email))
+                return new ValidationResult(GetErrorMessage());
+
+            return ValidationResult.Success;
+        }
+    }
+
+    public class Details
+    {
+        public bool blacklisted { get; set; }
+        public bool malicious_activity { get; set; }
+        public bool malicious_activity_recent { get; set; }
+        public bool spam { get; set; }
+        public bool suspicious_tld { get; set; }
+    }
+
+    public class Reputation
+    {
+        public Details details { get; set; }
+        public string email { get; set; }
+        public string reputation { get; set; }
+        public bool suspicious { get; set; }
+    }
+}
---
+++ Services/EmailAPICheck.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using VubUniversity.Models;
+
+namespace VubUniversity.Services
+{
+  
+        public class EmailAPICheck : IEmailCheckRisky
+        {
+            private readonly IConfiguration Configuration;
+            public EmailAPICheck(IConfiguration config)
+            {
+                Configuration = config;
+            }
+
+            public bool IsUntrusted(string emailAddress)
+            {
+            var emailRepApiKey = "7w06xw104v6aenrpco4ks8bq77qdvooeonzma2nsukvio5im";
+                HttpWebRequest repEmailRequest = (HttpWebRequest)WebRequest.Create($"https://emailrep.io/{emailAddress}");
+                repEmailRequest.Headers.Add("Cookie", $"{emailRepApiKey}");
+                repEmailRequest.Headers.Add("User-Agent", "MyAppName");
+                HttpWebResponse repEmailResponse = (HttpWebResponse)repEmailRequest.GetResponse();
+
+                Stream newStream = repEmailResponse.GetResponseStream();
+                var repEmail = new StreamReader(newStream).ReadToEnd();
+                var reputation = JsonSerializer.Deserialize<Reputation>(repEmail);
+
+                if (reputation.suspicious || reputation.details.blacklisted || reputation.details.spam || reputation.details.malicious_activity || reputation.details.malicious_activity_recent)
+                    return true;
+
+                return false;
+            }
+        }
+        public interface IEmailCheckRisky
+    {
+            bool IsUntrusted(string input);
+        }
+
+    
+}
